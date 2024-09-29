a167d14826c421d493dde4edf0da007d580bbdf8
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-09-29 13:20:26 +0200

    ADD ejer2, 3 AND 4

--- Fundamentos de la programacion/grupo3/practica2/Condicionales/EjerciciosPracticasCondicionales.h
+++ Fundamentos de la programacion/grupo3/practica2/Condicionales/EjerciciosPracticasCondicionales.h
@@ -3,3 +3,50 @@
 //
 #include <iostream>
 #include "functions.h"
+
+int ejer2(){
+  double num1, num2;
+
+  num1 = getDouble("Inserte un numero: ");
+  num2 = getDouble("Inserte otro numero: ");
+
+  if (num1 > num2) {
+    cout << "El número mayor es: " << num1 << endl;
+  } else if (num2 > num1) {
+    cout << "El número mayor es: " << num2 << endl;
+  } else {
+    cout << "Ambos números son iguales." << endl;
+  }
+
+  return 0;
+}
+
+int ejer3(){
+  double num1, num2, num3;
+
+  num1 = getDouble("Inserte el primer numero: ");
+  num2 = getDouble("Inserte el segundo numero: ");
+  num3 = getDouble("Inserte el tercer numero: ");
+
+  if (num1 > num2 && num1 > num3){
+    cout << "El número mayor es: " << num1 << endl;
+  } else if(num2 > num1 && num2 > num3){
+    cout << "El número mayor es: " << num2 << endl;
+  } else if(num3 > num1 && num3 > num2){
+    cout << "El número mayor es: " << num3 << endl;
+  } else {
+    cout << "Ambos números son iguales." << endl;
+  }
+
+  return 0;
+}
+
+int ejer4(){
+  int num;
+
+  num = getInt("Inserte un numero: ");
+
+  (num % 2 == 0) ? cout << "El número es par" : cout << "El número es impar";
+
+  return 0;
+}
