523eade5406530cce32cf21ecf56f6d0fa6fee4a
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-09-30 13:30:42 +0200

    ADD ejer2

--- Fundamentos de la programacion/grupo3/pratica3/EjerciciosCiclicas.h
+++ Fundamentos de la programacion/grupo3/pratica3/EjerciciosCiclicas.h
@@ -1,8 +1,24 @@
 //
 // Created by jmart on 30/09/2024.
 //
+#include <iostream>
+#include "functions.h"
+
+using namespace std;
 
 #ifndef EJERCICIOSCICLICAS_H
 #define EJERCICIOSCICLICAS_H
+
+int ejer2(){
+  int num;
+
+  num = getInt("Inserte un numero: ");
+
+  for(int i=0; i<=10; i++){
+    cout << num << " X " << i << " = " << num*i << endl;
+  }
+
+  return 0;
+}
 
 #endif //EJERCICIOSCICLICAS_H
