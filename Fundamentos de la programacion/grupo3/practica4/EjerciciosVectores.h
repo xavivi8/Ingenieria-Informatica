ffab17e078cf2f568bcc5b95edb11781e7753f99
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-10-12 12:05:31 +0200

    EjerciciosVectores ADD ejercicio1

--- Fundamentos de la programacion/grupo3/practica4/EjerciciosVectores.h
+++ Fundamentos de la programacion/grupo3/practica4/EjerciciosVectores.h
@@ -2,9 +2,38 @@
 // Created by jmart on 12/10/2024.
 //
 #include <iostream>
+#include <vector>
+
 #include "functions.h"
+
+using namespace std;
 
 #ifndef EJERCICIOSVECTORES_H
 #define EJERCICIOSVECTORES_H
+
+const int MAX_NUMEROS = 100;
+
+int ejercicio1(){
+  int numeros[MAX_NUMEROS];
+  int cantidad = 0;
+  int num = 0;
+
+  cout << "Inserte numeros hasta "<< MAX_NUMEROS <<" para almacenarlos (para salir inserte -1): " << endl;
+  do{
+    cin >> num;
+    if(num != -1) {
+      numeros[cantidad] = num;
+      cantidad++;
+    }
+
+  }while(num != -1 && cantidad < MAX_NUMEROS);
+
+  cout << "\nIndice\tValor\n";
+  for (int i = 0; i < cantidad; ++i) {
+    cout << (i+1) << "\t" << numeros[i] << "\n";
+  }
+
+  return 0;
+}
 
 #endif //EJERCICIOSVECTORES_H
