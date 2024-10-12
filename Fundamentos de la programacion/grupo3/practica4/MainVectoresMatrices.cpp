b45c0d4a381da9a7de14581132c78d2a07cb6e7a
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-10-12 12:05:55 +0200

    MainVectoresMatrices ADD Menu AND case 1

--- Fundamentos de la programacion/grupo3/practica4/MainVectoresMatrices.cpp
+++ Fundamentos de la programacion/grupo3/practica4/MainVectoresMatrices.cpp
@@ -1,3 +1,54 @@
 //
 // Created by jmart on 07/10/2024.
 //
+#include <iostream>
+#include <string>
+#include "functions.h"
+#include "EjerciciosVectores.h"
+
+using namespace std;
+
+std::string textoMenu()
+{
+    return "Seleccione el ejercicio que desea probar de secuenciales: "
+        "\nEjercicio 1: Almacenar y mostrar numeros con su indice."
+        "\nFin del programa: 0";
+}
+
+int menu()
+{
+    int opcion = 1;
+
+    do {
+        cout << "\n -----------------------------------------------------------------------------------" << endl;
+        cout << textoMenu() << endl;
+        opcion = getInt("Inserte la opcion: ");
+
+        switch (opcion)
+        {
+        case 0:
+            {
+                cout << "Fin" << endl;
+                break;
+            }
+        case 1:
+            {
+                ejercicio1();
+                break;
+            }
+        default:
+            {
+                cout << "Opcion invalida \n\n" << endl;
+                break;
+            }
+        }
+    } while (opcion != 0);
+
+    return 0;
+}
+
+int main()
+{
+    menu();
+    return 0;
+}
