42e93a6fb3931f572d9a061de0e79c0dae262830
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-11-22 12:13:09 +0100

    CHANGE

--- Fundamentos de la programacion/grupo3/practicaI/EjerciciosModular3.h
+++ Fundamentos de la programacion/grupo3/practicaI/EjerciciosModular3.h
@@ -1,8 +1,22 @@
 //
 // Created by jmart on 22/11/2024.
 //
+#include <iostream>
+
+#include "functions.h"
 
 #ifndef EJERCICIOSMODULAR3_H
 #define EJERCICIOSMODULAR3_H
+
+struct PERSONA {
+  std::string nombre;
+  int num_dni;
+  int edad;
+};
+
+int ejer1(){
+
+  return 0;
+}
 
 #endif //EJERCICIOSMODULAR3_H
