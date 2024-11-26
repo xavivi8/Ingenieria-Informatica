e0f3ef2f98570a4e662402a8377e8e71914a97e9
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-11-26 17:27:48 +0100

    CREATE MainPunteros.cpp

---
+++ Fundamentos de la programacion/grupo3/PracticaJ/MainPunteros.cpp
@@ -0,0 +1,45 @@
+//
+// Created by jmart on 26/11/2024.
+//
+#include <iostream>
+
+#include "functions.h"
+
+std::string textoMenu() {
+    return "Seleccione el ejercicio que desea probar de Punteros: "
+        "\nEjercicio 1: "
+        "\nFin del programa: 0";
+};
+
+int menu() {
+    int opcion;
+
+    do {
+        cout << "\n -----------------------------------------------------------------------------------" << endl;
+        cout << textoMenu() << endl;
+        opcion = getInt("Inserte la opcion: ");
+        cin.ignore();
+
+        switch (opcion)
+        {
+        case 0:
+            {
+                cout << "Fin" << endl;
+                break;
+            }
+        default:
+            {
+                cout << "Opcion invalida \n\n" << endl;
+                break;
+            }
+        }
+    } while (opcion != 0);
+
+    return 0;
+}
+
+int main() {
+    menu();
+    return 0;
+};
+
