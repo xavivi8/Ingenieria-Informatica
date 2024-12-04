5504aed72043970efaaad0e3d259280b5e6ae4a4
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-12-04 17:09:02 +0100

    CHANGE comments
    Co-Authored-By: Alex-pm72 <190608600+Alex-pm72@users.noreply.github.com>

--- Seguridad en tecnologias de la Informacion/Trabajo/DSA.py
+++ Seguridad en tecnologias de la Informacion/Trabajo/DSA.py
@@ -1,31 +1,36 @@
-from Crypto.PublicKey import DSA
-from Crypto.Signature import DSS
-from Crypto.Hash import SHA256
-import time
+from Crypto.PublicKey import DSA # Proporciona funcionalidad para generar claves DSA
+from Crypto.Signature import DSS #Gestiona las firmas y verificaciones según el estándar FIPS-186-3
+from Crypto.Hash import SHA256 # Algoritmo de has utilizado para crear un resumen del mensaje
+import time #Se usa para medir los tiempos de ejecucuón de distintas operaciones
 
 def evaluate_dsa(title, messageA):
     print(title)
+    '''
+    El código evalúa el rendimiento para las claves 1024, 2048 y 3072 bits, donde:
+        -Claves más grandes proporcionan mayor seguridad
+        -Claves más pequeñas son más rápidas en términos de generación t procesamiento
+    '''
     for key_size in [1024, 2048, 3072]:
         print(f"\nKey Size: {key_size} bits")
         
-        # Key generation
+        # Generación de clave
         start = time.time()
         key = DSA.generate(key_size)
         end = time.time()
         print(f"Key Generation Time: {end - start:.6f} seconds")
         
-        # Sign/Verify
+        # Firmar/Verificar
         message = messageA
         h = SHA256.new(message)
         
-        # Signing
+        # Firmar el hash del mensaje
         signer = DSS.new(key, 'fips-186-3')
         start = time.time()
         signature = signer.sign(h)
         end = time.time()
         print(f"Signing Time: {end - start:.6f} seconds")
         
-        # Verifying
+        # Verificar la firma
         verifier = DSS.new(key.publickey(), 'fips-186-3')
         start = time.time()
         verifier.verify(h, signature)
