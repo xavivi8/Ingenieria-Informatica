4e17538b6a2144e9244fbb00ce0bffd37dc16849
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2025-09-23 09:00:31 +0200

    Add initial implementation of VDinamico

--- SegundoAnyo/EDatos/Practicas/Práctica 1 Vector dinámico/VDinamico.h
+++ SegundoAnyo/EDatos/Practicas/Práctica 1 Vector dinámico/VDinamico.h
@@ -5,4 +5,41 @@
 #ifndef VDINAMICO_H
 #define VDINAMICO_H
 
+#include <algorithm>
+#include <cmath>
+#include <climits>
+
+
+template<class T>
+class VDinamico {
+private:
+    //Se ocupa de coger memoria
+    void allocateMemory();
+    //Se ocupa de liberar memoria
+    void freeMemory();
+    //Se ocupa de redondear a la potencia de 2
+    void roundToPowerOf2(unsigned long int &t);
+
+    unsigned long int tamal,tamaf;
+    T *v;
+
+public:
+    VDinamico();
+    VDinamico(unsigned long int tama);
+    VDinamico(const VDnamico<T> &orig);
+    VDinamico(const VDinamico<T>& origen, unsigned long int inicio, unsigned long int num);
+    ~VDinamico();
+
+    VDinamico<T> &operator=(const VDinamico<T> &arr);
+    T &operator[](unsigned long int pos);
+    void insert (const T &data, unsigned long int pos=UINT_MAX);
+    T remove (unsigned long int pos=UINT_MAX);
+    unsigned long int tama();
+
+    unsigned long int binarySearch(const T &data);
+
+    void sort();
+
+};
+
 #endif //VDINAMICO_H
