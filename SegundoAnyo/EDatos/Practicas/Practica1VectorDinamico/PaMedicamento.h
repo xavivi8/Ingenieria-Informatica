a5f948658efe9afa3ddd95e9d50b22a6aa83df57
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2025-09-26 12:39:39 +0200

    Add head

--- SegundoAnyo/EDatos/Practicas/Practica1VectorDinamico/PaMedicamento.h
+++ SegundoAnyo/EDatos/Practicas/Practica1VectorDinamico/PaMedicamento.h
@@ -5,9 +5,33 @@
 #ifndef PAMEDICAMENTO_H
 #define PAMEDICAMENTO_H
 
+#include <string>
+
 class PaMedicamento{
 private:
+    int id_num;
+    std::string id_alpha;
+    std::string nombre;
+
+public:
+    PaMedicamento();
+    PaMedicamento(int id_num, std::string id_alpha, std::string nombre);
+    ~PaMedicamento();
+
+    //Operators
+    bool &operator==(const PaMedicamento &object);
+    bool &operator<(const PaMedicamento &object);
     
+    //Getters
+    //Al poner al final el const se promete que esa funcion no modificara atributos
+    int getIdNum() const;
+    std::string getIdAlpha() const;
+    std::string getNombre() const;
+
+    //Setters
+    void setIdNum(int id_num);
+    void setIdAlpha(std::string id_alpha);
+    void setNombre(std::string nombre);
 };
 
 #endif //PAMEDICAMENTO_H
