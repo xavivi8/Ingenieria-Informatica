287766317d7aca3b456d02f0c76c33220ec91952
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2025-10-07 20:15:15 +0200

    Change

--- SegundoAnyo/ArquitecturaDeComputadores/Practicas/Practica2/ejercicio2.4.s
+++ SegundoAnyo/ArquitecturaDeComputadores/Practicas/Practica2/ejercicio2.4.s
@@ -4,18 +4,22 @@
 main:
     
     li s0, -1    # s0 = i contador exterior
-    li t0, 10    # limite del bucle exterior
-    li t1, 10    # limite del bucle interior
+    li t0, 10    # limite del bucle interno
 
 bucle_exterior:
     addi s0, s0, 1  
+    bgt s0, t0, fin
     li s1, -1    # s1 = j contador interior
+    
+    li a0, 10
+    li a7, 11
+    ecall
 
     
 bucle_interior:
     addi s1, s1, 1
     
-    #imprimo el valor de s0
+    #imprimo el valor de s1
     mv a0, s1
     li a7, 1
     ecall
@@ -30,7 +34,24 @@ bucle_interior:
     li a7, 1
     ecall
     
-    #imprimo el
+    #imprimo el =
     li a0, 61
     li a7, 11
+    ecall
+    
+    mul t1, s1, s0  # t0 = s1 * s0
+    mv a0, t1
+    li a7, 1
+    ecall
+    
+    #imprimo el _
+    li a0, 95
+    li a7, 11
+    ecall
+    
+    blt s1, t0, bucle_interior
+    beq s1, t0, bucle_exterior
+
+fin:
+    li a7, 10
     ecall
