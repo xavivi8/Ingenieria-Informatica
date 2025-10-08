6c96ce9a5b92aa8e47d1ede543442ac074db5308
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2025-10-08 19:05:25 +0200

    Add code

--- SegundoAnyo/EDatos/Practicas/Practica2Listas/src/Laboratorio.cpp
+++ SegundoAnyo/EDatos/Practicas/Practica2Listas/src/Laboratorio.cpp
@@ -13,3 +13,57 @@ Laboratorio::Laboratorio(){
     m_cod_postal = "";
     m_localidad = "";
 };
+
+Laboratorio::Laboratorio(int id, const std::string &nombre_lab, const std::string &direccion, const std::string &cod_postal, const std::string &localidad) {
+    m_id = id;
+    m_nombre_lab = nombre_lab;
+    m_direccion = direccion;
+    m_cod_postal = cod_postal;
+    m_localidad = localidad;
+}
+
+/**
+ * Getters
+ */
+int Laboratorio::getId() const {
+    return m_id;
+}
+
+std::string Laboratorio::getNombreLab() const {
+    return m_nombre_lab;
+}
+
+std::string Laboratorio::getDireccion() const {
+    return m_direccion;
+}
+
+std::string Laboratorio::getCodPostal() const {
+    return m_cod_postal;
+}
+
+std::string Laboratorio::getLocalidad() const {
+    return m_localidad;
+}
+
+/**
+ * Setters
+ */
+void Laboratorio::setId(int id) {
+    m_id = id;
+}
+
+void Laboratorio::setNombreLab(const std::string &nombre_lab) {
+    m_nombre_lab = nombre_lab;
+}
+
+void Laboratorio::setDireccion(const std::string &direccion) {
+    m_direccion = direccion;
+}
+
+void Laboratorio::setCodPostal(const std::string &cod_postal) {
+    m_cod_postal = cod_postal;
+}
+
+void Laboratorio::setLocalidad(const std::string &localidad) {
+    m_localidad = localidad;
+}
