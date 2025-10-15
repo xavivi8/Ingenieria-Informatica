1cf34dc846b5b7637d69e137e5955ac2863326c7
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2025-10-15 16:16:40 +0200

    Update MediExpress

--- SegundoAnyo/EDatos/Practicas/Practica2Listas/include/MediExpress.h
+++ SegundoAnyo/EDatos/Practicas/Practica2Listas/include/MediExpress.h
@@ -1,8 +1,21 @@
 //
 // Created by jmart on 08/10/2025.
 //
+#include "../include/VDinamico.h"
+#include "../include/ListaEnlazada.h"
+#include "../include/PaMedicamento.h"
+#include "../include/Laboratorio.h"
 
 #ifndef MEDIEXPRESS_H
 #define MEDIEXPRESS_H
+
+class MediExpress{
+    public:
+    void suministarMed(PaMedicamento paMed, Laboratorio lab);
+    void findLab() const;
+    void findLabCity() const;
+    void findCompuesto() const;
+ 
+};
 
 #endif //MEDIEXPRESS_H
