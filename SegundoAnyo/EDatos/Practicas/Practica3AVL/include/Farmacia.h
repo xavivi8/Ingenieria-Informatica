d912cacb37fd4e8452fd4cb77cbe4fd4deafe420
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2025-10-27 20:37:11 +0100

    Update Farmacia.h Add header

--- SegundoAnyo/EDatos/Practicas/Practica3AVL/include/Farmacia.h
+++ SegundoAnyo/EDatos/Practicas/Practica3AVL/include/Farmacia.h
@@ -5,4 +5,35 @@
 #ifndef FARMACIA_H
 #define FARMACIA_H
 
+#include "../include/PaMedicamento.h"
+#include "../include/Avl.h"
+#include <string>
+
+class MediExpress;
+class Farmacia{
+    std::string m_cif;
+    std::string m_provincia;
+    std::string m_localidad;
+    std::string m_nombre;
+    std::string m_direccion;
+    std::string m_codpostal;
+    Avl<PaMedicamento> dispense;
+
+    public:
+    Farmacia();
+    Farmacia(const std::string &cif,
+             const std::string &provincia,
+             const std::string &localidad,
+             const std::string &nombre,
+             const std::string &direccion,
+             const std::string &codPostal);
+    ~Farmacia();
+
+    bool operator<(const Farmacia &farma) const;
+
+    void pedidoMedicam(unsigned int id_num);
+    PaMedicamento* buscaMedicam(unsigned int id_num);
+    void dispensaMedicam(PaMedicamento pa);
+};
+
 #endif //FARMACIA_H
