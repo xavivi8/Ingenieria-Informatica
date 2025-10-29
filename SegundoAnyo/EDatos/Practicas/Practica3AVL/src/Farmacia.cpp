fe7532cc8c159bd929a8029e6047d64892c813e7
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2025-10-29 15:58:59 +0100

    Update Farmacia.cpp Add cnstructor

--- SegundoAnyo/EDatos/Practicas/Practica3AVL/src/Farmacia.cpp
+++ SegundoAnyo/EDatos/Practicas/Practica3AVL/src/Farmacia.cpp
@@ -1,4 +1,20 @@
 //
 // Created by jmart on 27/10/2025.
 //
+#include "../include/Farmacia.h"
 #include "../include/MediExpress.h"
+
+/**
+ * Constructor
+ */
+
+
+Farmacia::Farmacia(const std::string &cif, const std::string &province, const std::string &city, const std::string &name, const std::string &address, const std::string &postalCode){
+    m_cif = cif;
+    m_province = province;
+    m_city = city;
+    m_name = name;
+    m_address = address;
+    m_postalCode = postalCode;
+};
+
