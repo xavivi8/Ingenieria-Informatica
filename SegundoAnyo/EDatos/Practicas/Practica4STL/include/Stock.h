5b01146cb7d62e8d7ce78f0ae8a552fad95e3648
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2025-11-08 17:04:59 +0100

    Update Stock.h Add constructors

--- SegundoAnyo/EDatos/Practicas/Practica4STL/include/Stock.h
+++ SegundoAnyo/EDatos/Practicas/Practica4STL/include/Stock.h
@@ -9,6 +9,10 @@ class Stock{
     int m_id_PaMed;
     int m_num_stock;
 
+public:
+    Stock() = default;
+    Stock(int idPaMed, int unidades): m_id_PaMed(idPaMed), m_num_stock(unidades){};
+    
 };
 
 #endif //STOCK_H
