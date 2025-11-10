22653a0cce70b4c44066810eee1ed0624a5f8b6c
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2025-11-10 18:44:20 +0100

    Update Stock.h  restructure code

--- SegundoAnyo/EDatos/Practicas/Practica4STL/include/Stock.h
+++ SegundoAnyo/EDatos/Practicas/Practica4STL/include/Stock.h
@@ -1,6 +1,7 @@
 //
 // Created by jmart on 08/11/2025.
 //
+#include "../include/PaMedicamento.h"
 
 #ifndef STOCK_H
 #define STOCK_H
@@ -8,18 +9,25 @@
 class Stock{
     int m_id_PaMed;
     int m_num_stock;
+    PaMedicamento* m_idNum;
 
 public:
     Stock() = default;
-    Stock(int idPaMed, int unidades): m_id_PaMed(idPaMed), m_num_stock(unidades){};
+    Stock(int idPaMed, int unidades, PaMedicamento* pa);
+    virtual ~Stock();
 
-    void decrementa(int n);
+    bool decrementa(int n);
     void incrementa(int n);
 
+    bool operator<(const Stock &rhs) const;
+
+    void setIdPaMed(int n);
     void setNumStock(int n);
+    void setIdNum(PaMedicamento* pa);
 
     int getIdPaMed() const;
     int getNumStock() const;
+    PaMedicamento* getIdNum() const;
 };
 
 #endif //STOCK_H
