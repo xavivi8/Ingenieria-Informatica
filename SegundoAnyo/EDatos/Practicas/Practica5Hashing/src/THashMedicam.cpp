5af5b9ec10094b73cc0b4351ee16143dca7dc91b
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2025-11-23 17:53:18 +0100

    Update THashMedicam.cpp Implement operator=

--- SegundoAnyo/EDatos/Practicas/Practica5Hashing/src/THashMedicam.cpp
+++ SegundoAnyo/EDatos/Practicas/Practica5Hashing/src/THashMedicam.cpp
@@ -73,3 +73,19 @@ THashMedicam::THashMedicam(const THashMedicam &orig)
       primoMenor(orig.primoMenor),
       tabla(orig.tabla) {}
 
+/**
+ * Operadores
+ */
+
+THashMedicam& THashMedicam::operator=(const THashMedicam &orig) {
+    if (this != &orig) {
+        tamaf = orig.tamaf;
+        tamal = orig.tamal;
+        maxCol = orig.maxCol;
+        max10 = orig.max10;
+        sumaColisiones = orig.sumaColisiones;
+        primoMenor = orig.primoMenor;
+        tabla = orig.tabla;
+    }
+    return *this;
+}
