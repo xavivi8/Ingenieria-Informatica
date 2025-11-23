ef554df3c96b6b51ad93cd91f29d757c8854a03b
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2025-11-23 17:50:31 +0100

    Update THashMedicam.cpp Implement esprimo

--- SegundoAnyo/EDatos/Practicas/Practica5Hashing/src/THashMedicam.cpp
+++ SegundoAnyo/EDatos/Practicas/Practica5Hashing/src/THashMedicam.cpp
@@ -1,3 +1,22 @@
 //
 // Created by jmart on 21/11/2025.
 //
+#include "../include/THashMedicam.h"
+#include <cmath>
+
+/**
+ * Metodos privados 
+ */
+
+bool THashMedicam::esprimo(unsigned n) {
+    if (n < 2) return false;
+    if (n == 2 || n == 3) return true;
+    if (n % 2 == 0) return false;
+
+    unsigned limite = static_cast<unsigned>(std::sqrt(static_cast<double>(n)));
+    for (unsigned i = 3; i <= limite; i += 2) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
