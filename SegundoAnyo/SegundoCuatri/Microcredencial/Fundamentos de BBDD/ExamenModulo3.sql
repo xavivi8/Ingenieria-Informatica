6903c05a8be37d12978949e17dd42c5d1bd92702
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2026-02-10 20:55:16 +0100

    Update ExamenModulo3.sql

--- SegundoAnyo/SegundoCuatri/Microcredencial/Fundamentos de BBDD/ExamenModulo3.sql
+++ SegundoAnyo/SegundoCuatri/Microcredencial/Fundamentos de BBDD/ExamenModulo3.sql
@@ -48,6 +48,32 @@ LIMIT 3;
 
 # d
 
+# baratas
+
+SELECT
+  c.name AS categoria,
+  ROUND(SUM(p.amount) / NULLIF(COUNT(DISTINCT r.rental_id), 0), 2) AS precio_medio_alquiler
+FROM category c
+JOIN film_category fc ON fc.category_id = c.category_id
+JOIN inventory i      ON i.film_id      = fc.film_id
+JOIN rental r         ON r.inventory_id = i.inventory_id
+JOIN payment p        ON p.rental_id    = r.rental_id
+GROUP BY c.category_id, c.name
+ORDER BY precio_medio_alquiler ASC;
+
+# caras
+
+SELECT
+  c.name AS categoria,
+  ROUND(SUM(p.amount) / NULLIF(COUNT(DISTINCT r.rental_id), 0), 2) AS precio_medio_alquiler
+FROM category c
+JOIN film_category fc ON fc.category_id = c.category_id
+JOIN inventory i      ON i.film_id      = fc.film_id
+JOIN rental r         ON r.inventory_id = i.inventory_id
+JOIN payment p        ON p.rental_id    = r.rental_id
+GROUP BY c.category_id, c.name
+ORDER BY precio_medio_alquiler DESC;
+
 ## Ejercicio 2
 
 # a
@@ -108,3 +134,39 @@ FROM clientes_frecuentes cf
 JOIN datos_rankeados dr ON dr.customer_id = cf.customer_id AND dr.rk = 1
 GROUP BY cf.customer_id, cf.cliente, cf.email, cf.num_alquileres
 ORDER BY cf.num_alquileres DESC;
+
+## Ejercicio 3
+
+# a
+
+WITH tienda AS (
+	SELECT
+		i.store_id,
+		f.film_id,
+		f.title,
+		COUNT(*) AS num_alquileres
+	FROM rental r
+	JOIN inventory i ON i.inventory_id = r.inventory_id
+	JOIN film f ON f.film_id = i.film_id
+	GROUP BY i.store_id, f.film_id, f.title
+),
+ranked AS (
+	SELECT
+		store_id, film_id, title, num_alquileres,
+		DENSE_RANK() OVER (
+			PARTITION BY store_id
+			ORDER BY num_alquileres DESC
+		) AS rk
+	FROM tienda
+)
+SELECT
+	store_id, 
+    rk AS ranking_en_tienda, 
+    film_id, title, 
+    num_alquileres
+FROM ranked
+WHERE rk <= 10
+ORDER BY store_id, rk, title;
+
+# b
+
