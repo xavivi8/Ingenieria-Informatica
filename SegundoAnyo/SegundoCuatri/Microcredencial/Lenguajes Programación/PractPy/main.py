b033533456882a8e031965b21dfd48bbad1c6223
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2026-02-12 19:55:32 +0100

    A

--- SegundoAnyo/SegundoCuatri/Microcredencial/Lenguajes Programación/PractPy/main.py
+++ SegundoAnyo/SegundoCuatri/Microcredencial/Lenguajes Programación/PractPy/main.py
@@ -1,4 +1,6 @@
 # This is a sample Python script.
+import os
+
 
 # Press Mayús+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -14,3 +16,13 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+jose = os.getenv('mi_variable')
+
+if jose:
+    print('El valor del mi variable es: ', jose)
+else:
+    print('El valor de entorno de mi variable no está definida')
