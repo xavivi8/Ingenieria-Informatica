5ea0792c932ffd5e0b8f8ac8325f16b687d24c0f
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2026-03-10 09:27:35 +0100

    [hu15] Update main.java Add constructor parametrizado

--- SegundoAnyo/SegundoCuatri/DesarrolloAgil/pract_tdd/src/main/java/com/pract/tdd/Equipo.java
+++ SegundoAnyo/SegundoCuatri/DesarrolloAgil/pract_tdd/src/main/java/com/pract/tdd/Equipo.java
@@ -1,5 +1,23 @@
 package com.pract.tdd;
 
 public class Equipo {
-    
+    /// Nombre del equipo
+    String nombre;
+
+    /// Puntos del equipo
+    int puntos;
+
+    /**
+     * Constructor parametrizado.
+     * @param unNombre
+     * @param unosPuntos
+     * @throws IllegalArgumentException unNombre no puede ser vacío.
+     */
+    public Equipo(final String unNombre, final int unosPuntos) {
+        if (unNombre == null || unNombre == "")
+            throw (new IllegalArgumentException(
+                "Equipo: Ctor. parametrizado: Nombre no puede ser null ni cadena vacía."));
+        this.nombre = unNombre;
+        this.puntos = unosPuntos;
+    }
 }
