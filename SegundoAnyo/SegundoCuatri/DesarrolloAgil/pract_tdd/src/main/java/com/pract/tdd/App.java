392eb99a5b1bb5802b68cee6301589de469611f4
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2026-03-10 09:44:30 +0100

    [hu15] Update App Add imprimeClasificacion

--- SegundoAnyo/SegundoCuatri/DesarrolloAgil/pract_tdd/src/main/java/com/pract/tdd/App.java
+++ SegundoAnyo/SegundoCuatri/DesarrolloAgil/pract_tdd/src/main/java/com/pract/tdd/App.java
@@ -1,10 +1,31 @@
 package com.pract.tdd;
 
+import java.util.ArrayList;
+
 /**
  * Hello world!
  */
 public final class App {
     private App() {
+    }
+
+    /// Equipos que forman mi liga
+    static ArrayList<Equipo> misEquipos = new ArrayList<Equipo>();
+
+    /**
+     * Muestra en pantalla el listado de equipos, ordenado por los puntos que tienen.
+     */
+    final protected static void imprimeClasificacion() {
+        System.out.println("Clasificación:");
+        System.out.println("=============");
+
+        for (int i = 0; i < misEquipos.size(); ++i) {
+            System.out.println((i + 1) +
+                            ". " +
+                            misEquipos.get(i).getNombre() +
+                            "-" +
+                            misEquipos.get(i).getPuntos());
+        }
     }
 
     /**
