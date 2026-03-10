8ba55d45dcbbcf8648e68c5e76134fecbf72c14e
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2026-03-10 09:45:40 +0100

    [hu15] Update App salida por pantalla

--- SegundoAnyo/SegundoCuatri/DesarrolloAgil/pract_tdd/src/main/java/com/pract/tdd/App.java
+++ SegundoAnyo/SegundoCuatri/DesarrolloAgil/pract_tdd/src/main/java/com/pract/tdd/App.java
@@ -33,6 +33,10 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Mi liga");
+        System.out.println("");
+        System.out.println("");
+
+        imprimeClasificacion();
     }
 }
